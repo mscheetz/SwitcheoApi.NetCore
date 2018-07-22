8979c94e2250682d460ac3fed5ea30fddc79e626
Matt Scheetz <mfscheetz@gmail.com>
2018-07-21 21:52:08 -0400

    Updates to repo interface

--- SwitcheoApi.NetCore/SwitcheoApi.NetCore.Data.Interface/ISwitcheoRepository.cs
+++ SwitcheoApi.NetCore/SwitcheoApi.NetCore.Data.Interface/ISwitcheoRepository.cs
@@ -1,8 +1,46 @@
-﻿using System;
+﻿using SwitcheoApi.NetCore.Entities;
+using System;
+using System.Threading.Tasks;
 
 namespace SwitcheoApi.NetCore.Data.Interface
 {
     public interface ISwitcheoRepository
     {
+        /// <summary>
+        /// Get available currency pairs on Switcheo Exchange 
+        /// </summary>
+        /// <param name="bases">Base pairs to filter on (default all pairs)</param>
+        /// <returns>Array of trading pairs</returns>
+        Task<string[]> GetPairs(string[] bases = null);
+
+        /// <summary>
+        /// Get hashes of contracts deployed by Switcheo
+        /// </summary>
+        /// <returns>Array of Contracts</returns>
+        Task<Contract[]> GetContracts();
+
+        /// <summary>
+        /// Get candlestick chart data
+        /// </summary>
+        /// <param name="pair">Pair to filter</param>
+        /// <param name="interval">Time interval of candlestick</param>
+        /// <param name="endTime">End time of last candlestick (defaults to current UTC time)</param>
+        /// <param name="stickCount">Number of candlesticks to return (defaults to 10)</param>
+        /// <returns>Array of Candlesticks</returns>
+        Task<Candlstick[]> GetCandlesticks(string pair, Interval interval, long endTime = 0, int stickCount = 10);
+
+        /// <summary>
+        /// Get 24-hour data for all pairs and markets
+        /// </summary>
+        /// <returns>Array of Candlesticks</returns>
+        Task<Candlstick[]> GetLast24Hours();
+
+        /// <summary>
+        /// Get last price of given symbol
+        /// </summary>
+        /// <param name="symbols">String array of currency symbols (default null)</param>
+        /// <param name="bases">String array of base pairs (default null)</param>
+        /// <returns>Array LastPrices</returns>
+        Task<LastPrices[]> GetLastPrice(string[] symbols = null, string[] bases = null);
     }
 }
