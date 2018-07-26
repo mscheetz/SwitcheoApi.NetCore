96ae1b932089d9570c5e979a2b0df4a085276a94
Matt Scheetz <mfscheetz@gmail.com>
2018-07-25 21:20:09 -0400

    Create landing project

---
+++ SwitcheoApi.NetCore/SwitcheoApi.NetCore/SwitcheoApiClient.cs
@@ -0,0 +1,31 @@
+﻿using SwitcheoApi.NetCore.Data;
+using SwitcheoApi.NetCore.Data.Interface;
+using System;
+
+namespace Switcheo.NetCore
+{
+    public class SwitcheoApiClient
+    {
+        public ISwitcheoRepository SwitcheoRepository;
+
+        /// <summary>
+        /// Constructor, no authorization
+        /// </summary>
+        /// <param name="testRegion">Boolean to use test region (default = false)</param>
+        public SwitcheoApiClient(bool testRegion = false)
+        {
+            SwitcheoRepository = new SwitcheoRepository(testRegion);
+        }
+
+        /// <summary>
+        /// Constructor, with authorization
+        /// </summary>
+        /// <param name="address">Neo address</param>
+        /// <param name="privateKey">Neo address private key</param>
+        /// <param name="testRegion">Boolean to use test region (default = false)</param>
+        public SwitcheoApiClient(string address, string privateKey, bool testRegion = false)
+        {
+            SwitcheoRepository = new SwitcheoRepository(address, privateKey, testRegion);
+        }
+    }
+}
