d37e02af380907806d80cdcaace4142f82c5101f
Matt Scheetz <mfscheetz@gmail.com>
2018-08-30 20:12:31 -0400

    Client unit tests

---
+++ SwitcheoApi.NetCore/SwitcheoApi.NetCore/Tests/SwitcheoApiClientTests.cs
@@ -0,0 +1,59 @@
+﻿using Switcheo.NetCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using Xunit;
+
+namespace SwitcheoApi.NetCore.Tests
+{
+    public class SwitcheoApiClientTests
+    {
+        // Empty neo wallet for testing purposes only
+        private string _privateKey = "L3SDs1rP2Fs489VGFY4Lt2NAg3Km1PqJsBkQd4QsN8UvotGif1yZ";
+        private string _address = "AGA7VMVRpRDULskJ7sWsUt9YuhVj6CHz8y";
+        private string _scriptHash = "0x3161dab9941504e080db38f56ed9c722c7b43404";
+
+        public SwitcheoApiClientTests()
+        {
+        }
+
+        [Fact]
+        public void GetWallet_Test_ExceptionThrown()
+        {
+            var client = new SwitcheoApiClient();
+
+            Assert.Throws<Exception>(() => client.GetWallet());
+        }
+
+        [Fact]
+        public void GetWallet_Test_PKLoad()
+        {
+            var client = new SwitcheoApiClient(_privateKey);
+
+            var wallet = client.GetWallet();
+
+            Assert.True(wallet.address == _address);
+            Assert.True(wallet.scriptHash == _scriptHash);
+        }
+
+        [Fact]
+        public void GetWallet_Test_AddyLoad()
+        {
+            var client = new SwitcheoApiClient(_address);
+
+            var wallet = client.GetWallet();
+
+            Assert.True(wallet.scriptHash == _scriptHash);
+        }
+
+        [Fact]
+        public void GetWallet_Test_SCLoad()
+        {
+            var client = new SwitcheoApiClient(_scriptHash);
+
+            var wallet = client.GetWallet();
+
+            Assert.True(wallet.address == _address);
+        }
+    }
+}
