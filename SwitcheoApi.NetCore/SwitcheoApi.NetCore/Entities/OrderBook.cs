be0bbfcafbd69e8e28b8ab41895cc8344f371691
Matt Scheetz <mfscheetz@gmail.com>
2018-09-05 20:29:18 -0400

    new orderbook entities

---
+++ SwitcheoApi.NetCore/SwitcheoApi.NetCore/Entities/OrderBook.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SwitcheoApi.NetCore.Entities
+{
+    public class OrderBook : Offer
+    {
+        public SwitcheoOrder[] bids { get; set; }
+        public SwitcheoOrder[] asks { get; set; }
+    }
+}
---
+++ SwitcheoApi.NetCore/SwitcheoApi.NetCore/Entities/SwitcheoOrder.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SwitcheoApi.NetCore.Entities
+{
+    public class SwitcheoOrder : Offer
+    {
+        public decimal price
+        {
+            get
+            {
+                return this.want_amount / this.offer_amount;
+            }
+        }
+    }
+}
